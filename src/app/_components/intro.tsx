35050768fad246df18f681fba335a71cf3ea511e
mathidoSW <mathias.dovale@sellifyworks.com>
2025-09-22 19:21:08 +0200

    feat: Add blog post on Allbirds and Shopify eCommerce strategies in English and Spanish

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -9,7 +9,7 @@ export function Intro() {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight mt-16 md:pr-8 text-gray-900">
         {t('blog.title')}.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8 text-gray-600 max-w-md">
+      <h4 className="text-center md:text-left text-lg md:text-xl lg:text-2xl mt-5 md:pl-8 text-gray-600 max-w-md md:max-w-lg">
         {t('blog.subtitle')}
       </h4>
     </section>
