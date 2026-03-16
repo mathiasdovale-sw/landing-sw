02010a81ee03cb7b3f02e5726c00c4f04af98fd7
mathidoSW <mathias.dovale@sellifyworks.com>
2026-03-16 19:34:12 +0100

    fix: update Content Security Policy to include DoubleClick domains

--- middleware.ts
+++ middleware.ts
@@ -83,7 +83,7 @@ export function middleware(request: NextRequest) {
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: https: blob:",
-      "connect-src 'self' https://api.brevo.com https://a.klaviyo.com https://www.google-analytics.com https://analytics.google.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://log.cookieyes.com https://cdn-cookieyes.com https://connect.facebook.net https://www.facebook.com",
+      "connect-src 'self' https://api.brevo.com https://a.klaviyo.com https://www.google-analytics.com https://analytics.google.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://*.doubleclick.net https://log.cookieyes.com https://cdn-cookieyes.com https://connect.facebook.net https://www.facebook.com",
       "frame-src 'self' https://www.google.com",
       "object-src 'none'",
       "base-uri 'self'",
